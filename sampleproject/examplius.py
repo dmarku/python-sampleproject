a5c0ffb529c1d5c8b1e5d957aac21e91efbb1d2f
Markus Dittmann <dmarku@hrz.tu-chemnitz.de>
2017-05-24 15:45:32 +0200

    initial commit

---
+++ sampleproject/examplius.py
@@ -0,0 +1,4 @@
+def sample_print():
+    print('sample sample carrample')
+    print('demo demo sampularis!')
+    print('rex rex schwuppedischwapp')
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+from os import path
+
+setup(
+        name='sampleproject',
+        version='0.0.1',
+        description='a sample project to test out pip`s installation procedures',
+        long_description='bla bla bla',
+        author='Markus Dittmann',
+        author_email='dmarku@hrz.tu-chemnitz.de',
+        license='MIT',
+        packages=['sampleproject']
+)
